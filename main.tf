67597e08695ba0f279e29ade5ec576090844272c
cryodax <cryodax@gmail.com>
2022-03-30 21:27:49 +0000

    Added the first tag

--- main.tf
+++ main.tf
@@ -17,6 +17,7 @@ resource "azurerm_resource_group" "myresourcegroup" {
 
   tags = {
     environment = "Production"
+    department = "devops"
   }
 }
 
@@ -110,6 +111,9 @@ resource "azurerm_virtual_machine" "catapp" {
 
   network_interface_ids         = [azurerm_network_interface.catapp-nic.id]
   delete_os_disk_on_termination = "true"
+  tags = {
+    department = "devops"
+  }
 
   storage_image_reference {
     publisher = var.image_publisher
