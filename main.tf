979f398234e3d721e365cfa997fede8f867c3d3a
cryodax <cryodax@gmail.com>
2022-03-30 21:36:03 +0000

    added second tag

--- main.tf
+++ main.tf
@@ -112,7 +112,8 @@ resource "azurerm_virtual_machine" "catapp" {
   network_interface_ids         = [azurerm_network_interface.catapp-nic.id]
   delete_os_disk_on_termination = "true"
   tags = {
-    department = "devops"
+    Department = "devops"
+    Billable = "true"
   }
 
   storage_image_reference {
