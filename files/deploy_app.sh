9c5509ef56b1e4e4080028bf0f9993f808cbb689
Sky <63657556+cryodax@users.noreply.github.com>
2022-03-30 15:48:20 -0500

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Figure it out! 
+  Welcome to ${PREFIX}'s app. You're ten-ply,bud.
   <!-- END -->
   
   </div>
